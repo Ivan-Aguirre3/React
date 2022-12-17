eeed113b31524f7e5ccc76f620d42e607eb36860
Ivan-Aguirre3 <109114053+Ivan-Aguirre3@users.noreply.github.com>
2022-12-17 00:27:22 -0300

    Add files via upload

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,13 @@
+import { FaShoppingCart } from "react-icons/fa";
+
+const CartWidget = () => {
+  return (
+    <>
+      <p className="cart">
+        <FaShoppingCart />5
+      </p>
+    </>
+  );
+};
+
+export default CartWidget;
